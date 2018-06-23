41dd01bd529d153c0039fc5a8f10778fa99e4a13
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-23 14:08:03 +0200

    init

---
+++ webpod/webpod_client.js
@@ -0,0 +1,20 @@
+window.onload = start;
+
+function start() {
+
+    // webpod options
+    const options = [
+        {
+            theme: dark
+        },
+
+        {
+            position: bottom-right
+        }
+    ];
+
+    // webpod constructor
+
+    // new webpod
+    const webpod = new Webpod(options);
+}
