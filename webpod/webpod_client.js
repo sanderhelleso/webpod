5b642e5aa5d4305e874bdd801000df5bc4e0dff5
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-23 14:43:42 +0200

    init

--- webpod/webpod_client.js
+++ webpod/webpod_client.js
@@ -2,19 +2,36 @@ window.onload = start;
 
 function start() {
 
+    // webpod constructor
+    function Webpod(options) {
+        this.options = options;
+    }
+
     // webpod options
     const options = [
         {
-            theme: dark
+            theme: "dark"
         },
 
         {
-            position: bottom-right
+            position: "bottom-right"
         }
     ];
 
-    // webpod constructor
+    // webpod trigger
+    const trigger = document.createElement("a");
+    trigger.id = "webpodTrigger";
+    trigger.innerHTML = "<i class='fas fa-podcast'></i>";
+    trigger.addEventListener("click", openWebpod);
+    document.body.appendChild(trigger);
 
     // new webpod
-    const webpod = new Webpod(options);
+    const webpod = new Webpod(options, trigger);
+
+    console.log(webpod);
+
+    // open webpod
+    function openWebpod() {
+        console.log(webpod);
+    }
 }
